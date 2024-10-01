b661475762242a71966449f7cce1c9fc2a499b07
Daniel Rivera Yepes <danrivera505@gmail.com>
2024-09-30 22:22:26 -0500

    se actualiza el Proyecto

--- Dream_tracker/proyecto.py
+++ Dream_tracker/proyecto.py
@@ -5,31 +5,31 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='proyecto_final',
+            package='Dream_tracker',
             executable='img_publi',
             name='img_publi',
             output='screen',
         ),
         Node(
-            package='proyecto_final',
+            package='Dream_tracker',
             executable='img_emo',
             name='img_emo',
             output='screen',
         ),
         Node(
-            package='proyecto_final',
+            package='Dream_tracker',
             executable='img_microdreams',
             name='img_microdreams',
             output='screen',
         ),
         Node(
-            package='proyecto_final',
+            package='Dream_tracker',
             executable='img_tracking',
             name='img_tracking',
             output='screen',
         ),
         Node(
-            package='proyecto_final',
+            package='Dream_tracker',
             executable='img_displa',
             name='img_displa',
             output='screen',
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup
 
-package_name = 'proyecto_final'
+package_name = 'Dream_tracker'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'opencv-python',       
+        'mediapipe',           
+        'rclpy',               
+        'numpy',              
+    ],
     zip_safe=True,
-    maintainer='alejo',
-    maintainer_email='alejo@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Daniel Rivera',
+    maintainer_email='danrivera505@gmail.com',
+    description='blink, yawn and microsleep detector, using OpenCV, FaceMesh and ROS2.',
+    license='Apache License 2.0', 
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
